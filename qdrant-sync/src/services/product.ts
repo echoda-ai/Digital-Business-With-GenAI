cff1ad0ab4ed5ab948aaa1c24d79bf46f54e8d67
SEANGLAY <devolutionary.wizard@gmail.com>
2024-06-23 21:52:36 +0700

    Implemented daily task scheduling using cron and improved error handling

--- qdrant-sync/src/services/product.ts
+++ qdrant-sync/src/services/product.ts
@@ -1,5 +1,12 @@
 import knex from '../db/knex'
 
-export const getProducts = async () => {
-    return knex('products').select('*')
+export const getProducts = async (limit: number, offset: number) => {
+    return knex('products')
+        .select('products.productID',
+            'products.name',
+            'products.price',
+            'categories.categoryName')
+        .leftJoin('categories', 'products.categoryID', 'categories.categoryID')
+        .limit(limit)
+        .offset(offset)
 }
