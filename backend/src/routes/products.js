22df14e93dc1151e860d540f638b9e556202ec9a
SEANGLAY <devolutionary.wizard@gmail.com>
2024-06-30 19:31:14 +0700

    fixed: add verify token to get product by ids

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const ProductRepository = require('../repository/product');
 const { safeResponse, safeError } = require('../utils/response');
+const { jwtVerify: verifyToken } = require('../middleware/jwt-verify');
 
 const productRepository = new ProductRepository();
 
@@ -19,7 +20,7 @@ router.get('/:id', (req, res) => {
         .catch(error => safeError(res, { message: error.message }));
 });
 
-router.post('/get-product-by-ids', (req, res) => {
+router.post('/get-product-by-ids', verifyToken, (req, res) => {
     const { body } = req;
     productRepository.findProductById(body.productIds)
         .then(result => safeResponse(res, { payload: result }))
